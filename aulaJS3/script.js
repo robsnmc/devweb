7b740ae88a94116df8cd25d618b0565d6d15d3cc
Robson Machado Silva <robsnmchado@gmail.com>
2025-05-23 00:30:53 +0000

    Exemplo JS

---
+++ aulaJS3/script.js
@@ -0,0 +1,6 @@
+function calcular() {
+    soma.value = parseInt(valor1.value) + parseInt(valor2.value);
+    sub.value = parseInt(valor1.value) - parseInt(valor2.value);
+    divisao.value = parseInt(valor1.value) / parseInt(valor2.value);
+    mult.value = parseInt(valor1.value) * parseInt(valor2.value);
+}
